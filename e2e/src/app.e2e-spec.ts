4a50ebc42e07fbc1ddda69d6fcc7b142f8fe3eed
Sebastian Vargas <svargas9801@gmail.com>
2022-09-04 18:36:47 -0500

    Se sube el repositorio con las pruebas realizadas

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import { browser, logging, protractor } from 'protractor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -10,7 +10,62 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('pet-book app is running!');
+    expect(page.getTitleText()).toEqual('Pet Book Prueba Devops');
+  });
+
+  it('should display dogs when dogs button is clicked', async () => {
+    page.clickButton('perroButton');
+    const sources = await page.getImagesNames();
+    const namesApply  = sources.map(source => source.toLowerCase().includes('perro'));
+    let test = true;
+    namesApply.forEach(name => {
+      test = test && name;
+    });
+
+    expect(test).toBe(true);
+  });
+
+  it('should display cats when cat button is clicked', async () => {
+    page.clickButton('catButton');
+    const sources = await page.getImagesNames();
+    const namesApply  = sources.map(source => source.toLowerCase().includes('gato'));
+    let test = true;
+    namesApply.forEach(name => {
+      test = test && name;
+    });
+
+    expect(test).toBe(true);
+  });
+
+  it('should display cats and dogs when all button is clicked', async () => {
+    page.clickButton('allButton');
+    const sources = await page.getImagesNames();
+    const namesApply  = sources.map(source => (/perro|gato/i).test(source));
+    let test = true;
+    namesApply.forEach(name => {
+      test = test && name;
+    });
+
+    expect(test).toBe(true);
+  });
+
+  it('should display cats and dogs when all button is clicked', async () => {
+    page.clickButton('allButton');
+    const sources = await page.getImagesNames();
+    const namesApply  = sources.map(source => (/perro|gato/i).test(source));
+    let test = true;
+    namesApply.forEach(name => {
+      test = test && name;
+    });
+
+    expect(test).toBe(true);
+  });
+
+  it('should open the image that is randomly clicked', async () => {
+    const image = await page.clickRandomImage();
+    var EC = protractor.ExpectedConditions;
+    // Waits for the URL to contain 'foo'.
+    browser.wait(EC.urlContains(image), 5000);
   });
 
   afterEach(async () => {
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -6,6 +6,26 @@ export class AppPage {
   }
 
   getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+    return element(by.id('title')).getText() as Promise<string>;
+  }
+
+  clickButton(id: string) {
+    element(by.id(id)).click();
+  }
+
+  async clickRandomImage() {
+    const images = element.all(by.css('img'));
+    const numberOfImages = await images.count();
+    console.log(numberOfImages)
+    const imageSelected = Math.floor(Math.random() * (numberOfImages - 0 + 1) + 0)
+    images.last().click();
+    return await images.last().getAttribute('href');
+  }
+
+  getImagesNames(): Promise<string[]> {
+    return new Promise<string[]>(async (resolve, reject) => {
+      const names = await element.all(by.css('img')).map<string>(async (img) => await img.getAttribute('src'));
+      resolve(names);
+    })
   }
 }
